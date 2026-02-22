f6d9ce00aaae66b5f15da790c909bb6ef0f0627a
jfo <jfojfo@gmail.com>
2026-02-22 15:07:15 +0800

    train ok stable: transformer7 1. (posterior, attn_output) -> latent 2. attn_output[is_first_mask] = initial_state

--- sheeprl/algos/dreamer_ppo/dreamer_ppo.py
+++ sheeprl/algos/dreamer_ppo/dreamer_ppo.py
@@ -21,6 +21,7 @@ import sheeprl.algos.dreamer_ppo.agent_transformer3
 import sheeprl.algos.dreamer_ppo.agent_transformer4
 import sheeprl.algos.dreamer_ppo.agent_transformer5
 import sheeprl.algos.dreamer_ppo.agent_transformer6
+import sheeprl.algos.dreamer_ppo.agent_transformer7
 from sheeprl.algos.dreamer_v3.utils import prepare_obs, Moments, test
 from sheeprl.data.buffers import EnvIndependentReplayBuffer, SequentialReplayBuffer
 from sheeprl.envs.wrappers import RestartOnException
@@ -32,8 +33,8 @@ from sheeprl.utils.timer import timer
 from sheeprl.utils.utils import save_configs, Ratio
 
 
-F_BUILD_AGENT = sheeprl.algos.dreamer_ppo.agent_transformer6.build_agent
-F_TRAIN = sheeprl.algos.dreamer_ppo.agent_transformer6.train
+F_BUILD_AGENT = sheeprl.algos.dreamer_ppo.agent_transformer7.build_agent
+F_TRAIN = sheeprl.algos.dreamer_ppo.agent_transformer7.train
 SAMPLE_NEXT_OBS = False
 
 @register_algorithm()
