50fcd68a110e3d3b7d0ab67750e05117c07f7e15
jfo <jfojfo@gmail.com>
2026-03-19 08:14:55 +0800

    train fail: transformer9 - initial_state -> is_first, zeros -> continue start

--- sheeprl/algos/dreamer_ppo/agent_transformer9.py
+++ sheeprl/algos/dreamer_ppo/agent_transformer9.py
@@ -373,23 +373,23 @@ class WorldModel(nn.Module):
         attn_output, _, _ = self.transformer(embed_seq, embed_seq, attn_mask=attn_mask)
         return attn_output
 
-    # def _shift_and_mask(self, stochastic_state: Tensor, actions: Tensor, is_first: Tensor) -> Tensor:
-    #     initial_state = self.get_initial_states(stochastic_state.shape[:2])
-    #     stochastic_state = torch.cat((initial_state[:1], stochastic_state[:-1]), dim=0)
-    #     actions = torch.cat((torch.zeros_like(actions[:1]), actions[:-1]), dim=0)
-    #     # 对于is_first为1的位置，重置为初始状态
-    #     stochastic_state = (1 - is_first.unsqueeze(-1)) * stochastic_state + is_first.unsqueeze(-1) * initial_state
-    #     actions = (1 - is_first) * actions
-    #     return stochastic_state, actions
-
     def _shift_and_mask(self, stochastic_state: Tensor, actions: Tensor, is_first: Tensor) -> Tensor:
-        continue_state = self.get_initial_states([1, stochastic_state.shape[1]])
-        stochastic_state = torch.cat((continue_state[:1], stochastic_state[:-1]), dim=0)
+        initial_state = self.get_initial_states(stochastic_state.shape[:2])
+        stochastic_state = torch.cat((torch.zeros_like(stochastic_state[:1]), stochastic_state[:-1]), dim=0)
         actions = torch.cat((torch.zeros_like(actions[:1]), actions[:-1]), dim=0)
-        # 对于is_first为1的位置，重置为zero（seq staring token）
-        stochastic_state = (1 - is_first.unsqueeze(-1)) * stochastic_state
+        # 对于is_first为1的位置，重置为初始状态
+        stochastic_state = (1 - is_first.unsqueeze(-1)) * stochastic_state + is_first.unsqueeze(-1) * initial_state
         actions = (1 - is_first) * actions
         return stochastic_state, actions
+
+    # def _shift_and_mask(self, stochastic_state: Tensor, actions: Tensor, is_first: Tensor) -> Tensor:
+    #     continue_state = self.get_initial_states([1, stochastic_state.shape[1]])
+    #     stochastic_state = torch.cat((continue_state[:1], stochastic_state[:-1]), dim=0)
+    #     actions = torch.cat((torch.zeros_like(actions[:1]), actions[:-1]), dim=0)
+    #     # 对于is_first为1的位置，重置为zero（seq staring token）
+    #     stochastic_state = (1 - is_first.unsqueeze(-1)) * stochastic_state
+    #     actions = (1 - is_first) * actions
+    #     return stochastic_state, actions
 
     def _representation(self, embedded_obs: Tensor) -> Tuple[Tensor, Tensor]:
         logits = self.representation_model(embedded_obs)
